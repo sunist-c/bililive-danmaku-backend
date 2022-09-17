69f0fa8895057cb40beafd4c1986057f4d96b2a9
sunist-c <sunist@mail.sunist.cn>
2022-09-17 16:14:13 +0800

    add main model

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sunist-c/bililive-danmaku/model"
+	"github.com/sunist-c/bililive-danmaku/model/pool"
+	"github.com/sunist-c/bililive-danmaku/service/api"
+	"github.com/sunist-c/bililive-danmaku/websocket"
+	"log"
+)
+
+func main() {
+	model.WaitGroup.Add(1)
+	if model.GetGlobalConfig().BackendMode {
+		api.StartHttpService()
+	} else {
+		log.Printf("Please input your room-id\n")
+		var room uint32
+		_, _ = fmt.Scanf("%d", &room)
+		if room == 0 {
+			log.Printf("Bad room-id, exit...\n")
+			return
+		} else {
+			log.Printf("Try to connect to room %d\nYou can exit the program by type 'exit' any time\n", room)
+
+			client := websocket.NewClientWithHandler(room, pool.EmptyHandler())
+			success := false
+			for i := 0; i < 10; i++ {
+				success = client.Serve()
+				if success {
+					break
+				}
+			}
+
+			go func() {
+				var x string
+				_, _ = fmt.Scanf("%v\n", &x)
+				if x == "exit" || x == "exit\n" || x == "exit\r\n" {
+					model.WaitGroup.Done()
+				}
+			}()
+		}
+	}
+	model.WaitGroup.Wait()
+}
