e85034632a26141cc9743a85baec515193ef74c4
sunist-c <sunist@mail.sunist.cn>
2022-09-19 16:38:45 +0800

    simplified code

--- service/backend/exit.go
+++ service/backend/exit.go
@@ -1,17 +1,44 @@
 package backend
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/sunist-c/bililive-danmaku-backend/model"
-	"time"
+	"os"
+
+	"github.com/sunist-c/bililive-danmaku-backend/common/logging"
 )
 
-func ExitHandler() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.JSON(200, nil)
-		defer func() {
-			time.Sleep(time.Second)
-			model.WaitGroup.Done()
-		}()
+var (
+	exitService *ExitService
+)
+
+func init() {
+	exitService = &ExitService{
+		channelService: GetDefaultChannelService(),
 	}
+
+	exitService.Serve()
+}
+
+func GetDefaultExitService() *ExitService {
+	return exitService
+}
+
+type ExitService struct {
+	channelService *ChannelService
+}
+
+func (s *ExitService) Exit() {
+	logging.Info("execute exit operation")
+	for i := 0; i < 3; i++ {
+		success, total := s.channelService.RemoveAllChannel()
+		if total == success {
+			return
+		}
+		logging.Warn("failed to remove all channels, closed %v, total %v, retrying...", success, total)
+	}
+	logging.Info("closed all channels, have a good day!")
+	os.Exit(0)
+}
+
+func (s *ExitService) Serve() {
+
 }
